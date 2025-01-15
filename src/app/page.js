ee2b5bab84bc908a4b6b4757ae0ce15d1014a4ac
Johannes Tiyasi <theanthem8@gmail.com>
2025-01-15 11:00:27 +0200

    Add floating properties to capture button

--- src/app/page.js
+++ src/app/page.js
@@ -61,38 +61,57 @@ const CameraCapture = () => {
   };
 
   return (
-    <div className="relative h-screen">
-      {/* <h1 className="text-center text-2xl font-semibold my-4">
+    <>
+      <div className="h-screen">
+        {/* <h1 className="text-center text-2xl font-semibold my-4">
         Evidence capture
       </h1> */}
 
-      <div className="mx-5 mt-5 bg-white border border-gray-300 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-        <video
-          ref={videoRef}
-          autoPlay
-          playsInline
-          id="player"
-          className="w-full h-96 object-cover rounded-lg"
+        <div className="mx-5 mt-5 bg-white border border-gray-300 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+          <video
+            ref={videoRef}
+            autoPlay
+            playsInline
+            id="player"
+            className="w-full h-96 object-cover rounded-lg"
+          />
+        </div>
+
+        {/* Horizontal Scroll for Captured Images */}
+        <div className="flex overflow-x-auto mt-4 py-2 space-x-2.5 px-5">
+          {capturedImages.map((image, index) => (
+            <div
+              key={index}
+              className="flex-shrink-0 w-20 h-20 border border-gray-300 rounded-lg overflow-hidden"
+            >
+              <img
+                src={image}
+                alt={`Captured ${index}`}
+                className="object-cover w-full h-full"
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Capture Button */}
+        {/* <button
+        onClick={captureImage}
+        id="capture"
+        className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white font-semibold py-2 px-6 rounded-full shadow-md hover:bg-blue-600"
+      >
+        Capture
+      </button> */}
+
+        <canvas
+          ref={canvasRef}
+          id="canvas"
+          style={{
+            display: "none", // Hidden by default
+            marginTop: "20px",
+            border: "1px solid #ccc",
+          }}
         />
       </div>
-
-      {/* Horizontal Scroll for Captured Images */}
-      <div className="flex overflow-x-auto mt-4 py-2 space-x-2.5 px-5">
-        {capturedImages.map((image, index) => (
-          <div
-            key={index}
-            className="flex-shrink-0 w-20 h-20 border border-gray-300 rounded-lg overflow-hidden"
-          >
-            <img
-              src={image}
-              alt={`Captured ${index}`}
-              className="object-cover w-full h-full"
-            />
-          </div>
-        ))}
-      </div>
-
-      {/* Capture Button */}
       <button
         onClick={captureImage}
         id="capture"
@@ -100,17 +119,7 @@ const CameraCapture = () => {
       >
         Capture
       </button>
-
-      <canvas
-        ref={canvasRef}
-        id="canvas"
-        style={{
-          display: "none", // Hidden by default
-          marginTop: "20px",
-          border: "1px solid #ccc",
-        }}
-      />
-    </div>
+    </>
   );
 };
 
